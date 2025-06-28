6be0bf6356af7f2079c834415dc5bfe62e1f3774
Muthuraj-coder <muthurajias3232@gmail.com>
2025-06-28 11:16:41 +0530

    Initial

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\muthu\OneDrive\Documents\Python_Project\payroll_web\venv
+set VIRTUAL_ENV=D:\Payroll_Python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
